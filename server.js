d67dd72fe8ede17b18edaa08b6b8343dc56e4807
Calah Hansen <calah.hansen@gmail.com>
2019-10-09 12:42:27 -0500

    moved axios.get into the app.get to scrape website

--- server.js
+++ server.js
@@ -1,85 +1,90 @@
 // Dependencies
 const express = require("express");
-// const mongojs = require("mongojs"); //Change this to mongoose?? Already installed mongoose
+const logger = require("morgan");
+// const mongojs = require("mongojs");
 const mongoose = require('mongoose');
 
 // Require axios and cheerio. This makes the scraping possible
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+//Require all models
+const db = require("./models");
+
+const PORT = 3000;
+
 // Initialize Express
 const app = express();
 
-// Database configuration
-const databaseUrl = "scraper";
-const collections = ["scrapedData"];
+//Configure middleware
 
-// Hook mongojs configuration to the db variable
-// const db = mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
- 
-mongoose.connect('mongodb://localhost/my_database', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+app.use(logger("dev"));
+//Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Static Folder is "public"
+app.use(express.static("public"));
 
-
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
+//Connect to Mongo DB 
+mongoose.connect('mongodb://localhost/newsDB', {
+  useNewUrlParser: true
 });
 
-// TODO: make two more routes
 
-// Route 1
-// =======
-// This route will retrieve all of the data
-// from the scrapedData collection as a json (this will be populated
-// by the data you scrape using the next route)
+// Main route (GET to scrape the badassquilters website)
+app.get("/scrape", function (req, res) {
+  // Make a request via axios to grab the HTML body from the site of your choice
+  axios.get("https://www.badassquilterssociety.com/basically-badass-3/").then(function (response) {
 
-// Route 2
-// =======
-// When you visit this route, the server will
-// scrape data from the site of your choice, and save it to
-// MongoDB.
-// TIP: Think back to how you pushed website data
-// into an empty array in the last class. How do you
-// push it into a MongoDB collection instead?
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    const $ = cheerio.load(response.data);
 
-/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+    // Select each element in the HTML body from which you want information.
+    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+    // but be sure to visit the package's npm page to see how it works
+    $("article").each(function (i, element) {
+      //Save the results object
+      const results = {};
+//Add the title and link and save as properties/keys in the array
+      results.title = $(this).children("a").text(); //this is referring to element I think...
+      results.link = $(this).children("a").attr("href");
 
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
-
-// Make a request via axios to grab the HTML body from the site of your choice
-axios.get("https://www.badassquilterssociety.com/basically-badass-3/").then(function(response) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  const $ = cheerio.load(response.data);
-
-  // An empty array to save the data that we'll scrape
-  const results = [];
-
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("article").each(function(i, element) {
-
-    const title = $(element).find("a").text();
-    const link = $(element).find("a").attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
+      // Save these results and push into result array
+      results.push({
+        title: title,
+        link: link
+      });
     });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results);
+
   });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-  
-});
+
+
+  // TODO: make two more routes
+
+  // Route 1
+  // =======
+  // This route will retrieve all of the data
+  // from the scrapedData collection as a json (this will be populated
+  // by the data you scrape using the next route)
+
+  // Route 2
+  // =======
+  // When you visit this route, the server will
+  // scrape data from the site of your choice, and save it to
+  // MongoDB.
+  // TIP: Think back to how you pushed website data
+  // into an empty array in the last class. How do you
+  // push it into a MongoDB collection instead?
+
+  /* -/-/-/-/-/-/-/-/-/-/-/-/- */
+
+  // Listen on port 3000
+  app.listen(3000, function () {
+    console.log("App running on port 3000!");
+  });
+
